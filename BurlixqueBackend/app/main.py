0e7e1e8487c2cab02120e2bca15c5c47e1e9ae47
Daniel Olaleye <111056400+Ephitah@users.noreply.github.com>
2025-01-31 07:53:00 +0100

    Update main.py
    Allowed all origins

--- BurlixqueBackend/app/main.py
+++ BurlixqueBackend/app/main.py
@@ -11,12 +11,12 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "https://burlixque-rho.vercel.app/",
+    "https://burlixque-rho.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
